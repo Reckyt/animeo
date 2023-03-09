12270b33fbc27c401be064776a39003b2a8029f7
Maxime LOMBARDO <mlombardo@topinvest.fr>
2023-03-09 18:35:48 +0100

    création du composant home et du module animeo avec son typage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ButtonComponent } from './button/button.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
