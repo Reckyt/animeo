c58ac2da6beac659eac616f1da514f2686f35c2a
cvacant <cvacant@topinvest.fr>
2023-03-23 17:55:12 +0100

    structure page de login*

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'home',component: HeaderComponent ,canActivate: [AuthGuard]},
+  { path: '', redirectTo: 'login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: 'login.component.html',
+  styleUrls: ['login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  message: string = ' Vous êtes déconnecté.';
+  email: string;
+  password: string;
+  auth: AuthService;
+
+  constructor( 
+    private authService: AuthService, 
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.auth = this.authService;
+  }
+
+  setMessage() {
+    if(this.auth.isLoggedIn){
+      this.message= 'Vous êtes connecté.'
+    } else {
+      this.message= 'Identifiant ou mot de passe incorrect.'
+    }
+  }
+
+  login() {
+    this.message = "Tentative de connexion en cours ..."
+    this.auth.login(this.email, this.password)
+    .subscribe((isLoggedIn: boolean) => {
+      this.setMessage();
+      console.log('🏞🎆🏂 ~ LoginComponent ~ isLoggedIn => ', isLoggedIn);
+      if(isLoggedIn){
+        this.router.navigate(['/home']);
+      }else{
+        this.password = '';
+        this.router.navigate(['login']);
+      }
+    })
+  }
+
+  logout() {
+    this.auth.logout();
+    this.message = 'Vous êtes déconnecté.'
+  }
+
+}
